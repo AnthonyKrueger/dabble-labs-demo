fd6017c39fdaa78122fd7eae7b1a8ff1113233f5
aik <anthonykrueger0@gmail.com>
2021-08-26 10:34:50 -0500

    base server created, mongoose model made

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const popSchema = new Schema({
+    country: {
+        type: String
+    },
+    year: {
+        type: Number
+    },
+    area: {
+        type: Number
+    },
+    population: {
+        type: Number
+    }
+})
+
+const PopData = model('PopData', popSchema);
+
+module.exports = PopData;
